225240adce844651f9830c572a20b0d477df8304
achermack <achermack@wisc.edu>
2022-08-04 00:12:24 -0500

    fix: Begin user page buildout

---
+++ pages/l/[...hash].vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex h-screen bg-slate-800">
+    <div class="m-auto">
+      <div class="card bg-white text-slate-800 align-middle">
+        <div class="card-body w-96">
+          <h2 class="card-title">{{ hash }}</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: "User Link Page",
+  data() {
+    return {
+      hash: this.$route.params.hash[0],
+    };
+  },
+};
+</script>
